42b06646efd1c753901aa228583245c369da2323
DHINESH <122241723+dhinesh-mohan@users.noreply.github.com>
2023-11-18 14:06:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,17 +21,17 @@ async function checkWeather(city) {
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/hr";
 
-    if (data.weather[0].main == "Clouds") weatherIcon.src = "images/clouds.png";
+    if (data.weather[0].main == "Clouds") weatherIcon.src = "./images/clouds.png";
     else if (data.weather[0].main == "Clear")
-      weatherIcon.src = "images/clear.png";
+      weatherIcon.src = "./images/clear.png";
     else if (data.weather[0].main == "Drizzle")
-      weatherIcon.src = "images/Drizzle.png";
+      weatherIcon.src = "./images/Drizzle.png";
     else if (data.weather[0].main == "Mist")
-      weatherIcon.src = "images/Mist.png";
+      weatherIcon.src = "./images/Mist.png";
     else if (data.weather[0].main == "Rain")
-      weatherIcon.src = "images/rain.png";
+      weatherIcon.src = "./images/rain.png";
     else if (data.weather[0].main == "Snow")
-      weatherIcon.src = "images/snow.png";
+      weatherIcon.src = "./images/snow.png";
 
     document.querySelector(".weather").style.display = "block";
     document.querySelector(".error").style.display = "none";
